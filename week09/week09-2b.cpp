3d36c4ed1050e99f035468670fd231193eab7750
xinxinchu <144758431+xinxinchu@users.noreply.github.com>
2024-11-07 16:30:06 +0800

    week09

---
+++ week09/week09-2b.cpp
@@ -0,0 +1,13 @@
+int diagonalSum(int** mat, int matSize, int* matColSize) {
+
+        int M = matSize;
+        int ans = 0;
+        for(int i=0; i<M; i++)
+        {
+            ans += mat[i][i]; //左上、右下
+            ans += mat[i][M-1-i]; //右上、左下
+        }
+        //扣掉中間
+        if(M%2==1) ans -= mat[M/2][M/2];
+        return ans;
+}
